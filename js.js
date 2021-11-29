11566a2b640eb7ca9a0e0f5a9146ce65b2557d2b
NguyenHongThinh-2003 <ocbu0212@gmail.com>
2021-11-30 06:49:53 +0700

    ht

--- js.js
+++ js.js
@@ -1,7 +1,8 @@
 function tinhDiem() {
 
     document.getElementById("img").style.transform = "translateX(100%)";
-    document.getElementById("rs").style.transform = "translateY(-200%)";
+    document.getElementById("rs").style.transform = "translateY(-280%)";
+    document.getElementById("img").style.clipPath = "polygon(40% 0%, 40% 20%, 100% 20%, 100% 80%, 40% 80%, 40% 100%, 0% 50%)";
     document.getElementById("img").style.transition = " all 0.9s"
     document.getElementById("rs").style.transition = " all 0.9s"
     var toan = document.getElementById('toan').value;
@@ -12,7 +13,7 @@ function tinhDiem() {
     diemHoa = Number(hoa)
 
     var dtb = (diemToan + diemli + diemHoa) / 3;
-    document.getElementById('rs').textContent = (" ĐTB là: " + dtb)
+    document.getElementById('rs').textContent = (" ĐTB là: " + dtb.toFixed(0))
     document.getElementById('rs').style.opacity = "1"
 
 
@@ -21,6 +22,7 @@ function reset() {
 
     document.getElementById("img").style.transform = "translateX(0)";
     document.getElementById("rs").style.transform = "translateY(-180px)";
+    document.getElementById("img").style.clipPath = "none";
 
     document.getElementById('rs').textContent = ""
 
